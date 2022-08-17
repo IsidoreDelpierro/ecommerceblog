65b9ed16139c47a3f4789d5139bb60d5abe657f1
Isidore DelPierro <fontadore@gmail.com>
2022-08-17 14:44:23 +0100

    Create super user.
    Install app in settings.py
    
    Configure routing in url.py & url.py
    
    Create home view
    
    Create home.html template
    
    Create Post model 
    
    Register Post model in admin.py

--- blog/models.py
+++ blog/models.py
@@ -1,3 +1,16 @@
+from tabnanny import verbose
 from django.db import models
+from django.contrib.auth.models import User 
 
 # Create your models here.
+
+class Post(models.Model):
+    title = models.CharField(max_length=255)
+    author = models.ForeignKey(User, on_delete=models.CASCADE)
+    body = models.TextField() 
+
+    class Meta:
+        verbose_name_plural = "Posts"
+
+    def __str__(self):
+        return self.title + ' | ' + str(self.author)  
