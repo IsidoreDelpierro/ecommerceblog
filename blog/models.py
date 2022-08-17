c748cc22e6d4b230c17090e0e732d5283d129ef6
Isidore DelPierro <fontadore@gmail.com>
2022-08-17 16:23:09 +0100

    Basic Styling
    navbar.html included in main.html,
    
    main.html extended in every page, 
    
    title_tag for dynamic titles, 
    
    meta_tag for SEO

--- blog/models.py
+++ blog/models.py
@@ -6,6 +6,8 @@ from django.contrib.auth.models import User
 
 class Post(models.Model):
     title = models.CharField(max_length=255)
+    title_tag = models.CharField(max_length=255, default="eCommerce Blog")
+    meta_tag = models.CharField(max_length=255, default="kaavi")
     author = models.ForeignKey(User, on_delete=models.CASCADE)
     body = models.TextField() 
 
