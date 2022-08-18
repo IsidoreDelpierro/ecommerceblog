1dc3ac3a4ac83f8c193fb6a12a47ccd8bd874c4e
Isidore DelPierro <fontadore@gmail.com>
2022-08-18 15:25:23 +0100

    Add Blog Category Navbar Links ----------------------------------
    1) Add bootstrap dropdown to navbar
    
    2) Pull from db to make dropdown dynamic
    
    3) Add get_context_data to HomeView to make the categories dropdown
    
    4) Add this method to every view on which we want the categories 
    dropdown
    
    5) Create CategoryListView and use it to create a Categories page
    
    6) Create category_list.html to display categories
    
    7) Add the necessary routing (category_list)

--- blog/models.py
+++ blog/models.py
@@ -26,7 +26,7 @@ class Post(models.Model):
     author = models.ForeignKey(User, on_delete=models.CASCADE)
     body = models.TextField() 
     post_date = models.DateField(auto_now_add=True)
-    category = models.CharField(max_length=255, default="uncategorized")
+    category = models.CharField(max_length=255, default="Scooter")
 
     class Meta:
         verbose_name_plural = "Posts"
--- blog/views.py
+++ blog/views.py
@@ -17,6 +17,17 @@ class HomeView(ListView):
     ordering = ['-post_date']
     #ordering = ['-id']
 
+    def get_context_data(self, *args, **kwargs):
+        cat_menu = Category.objects.all()
+        context = super(HomeView, self).get_context_data(*args, **kwargs)
+        context["cat_menu"] = cat_menu
+        return context 
+
+
+def CategoryListView(request):
+    cat_menu_list = Category.objects.all()
+    context = {'cat_menu_list':cat_menu_list}
+    return render(request, 'blog/category_list.html', context)
 
 def CategoryView(request, cats):
     category_posts = Post.objects.filter(category=cats.replace('-', ' '))
