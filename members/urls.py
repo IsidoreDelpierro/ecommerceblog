7c3f929faf0bd6d4b4d739a274b66867ec8100eb
Isidore DelPierro <fontadore@gmail.com>
2022-08-18 09:08:14 +0100

    User Authentication -------------------
    1) Create separate app (members) to handle user authentication
    
    2) Install it in settings.py and create its routing file urls.py
    
    3) Include members.urls in project router
    
    4) Create login.html and registration.html in members app
    
    5) Make necessary imports and create UserRegistrationView in 
    members.view.py
    
    6) Update routing for members app
    
    7) Update navbar to include Register and Login
    
    8) Set LOGIN_REDIRECT_URL and LOGOUT_REDIRECT_URL in settings.py

--- members/urls.py
+++ members/urls.py
@@ -1,5 +1,6 @@
 from django.urls import path 
+from .views import UserRegistrationView
 
 urlpatterns = [
-    path('', HomeView.as_view(), name="home");
+    path('register/', UserRegistrationView.as_view(), name="register"),
 ]
