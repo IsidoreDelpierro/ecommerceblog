60ffd945f888ee60d5bef306842c13a25d325549
Isidore DelPierro <fontadore@gmail.com>
2022-08-18 08:35:03 +0100

    Order By Date ------------- 1) Update Post model to add post_date field
    2) Display post_date on ListView and DetailView templates

--- blog/views.py
+++ blog/views.py
@@ -14,7 +14,8 @@ from django.urls import reverse_lazy
 class HomeView(ListView):
     model = Post 
     template_name = "blog/home.html" 
-    ordering = ['-id']
+    ordering = ['-post_date']
+    #ordering = ['-id']
 
 
 class ArticleDetailView(DetailView):
