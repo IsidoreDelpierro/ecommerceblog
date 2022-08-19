c622e42c9bc3edc60964978d405c4c60ef71e289
Isidore DelPierro <fontadore@gmail.com>
2022-08-19 09:56:22 +0100

    Style Profile Page With Bootstrap
    ---------------------------------------------
    
    
    1) Create EditProfileForm in forms.py of members app passing in 
    UserChangeForm 
    
    2) Import it in views.py and use it in UserEditView

--- members/views.py
+++ members/views.py
@@ -2,7 +2,7 @@ from django.shortcuts import render
 from django.views import generic
 from django.contrib.auth.forms import UserCreationForm, UserChangeForm
 from django.urls import reverse_lazy
-from .forms import SignUpForm
+from .forms import SignUpForm, EditProfileForm
 
 # Create your views here.
 
@@ -13,9 +13,9 @@ class UserRegistrationView(generic.CreateView):
 
 
 class UserEditView(generic.UpdateView):
-    form_class = UserChangeForm
+    form_class = EditProfileForm
     template_name = "registration/edit_profile.html"
     success_url = reverse_lazy('home')
 
-    def get_object(self): 
+    def get_object(self):
         return self.request.user
