39ca4249b34bc16bc92526507c773aa66d2b6a88
Isidore DelPierro <fontadore@gmail.com>
2022-08-19 08:51:46 +0100

    Add Rich Text Editor To Django Blog -------------------------------------
    1) pip install django-ckeditor
    
    2) from ckeditor.fields import RichTextField (in models.py)
    
    3) body = RichTextField(blank=True, null=True)
    
    4) Make migrations & migrate
    
    5) Install ckeditor in settings.py
    
    6) Add {{form.media}} above {{form.as_p}} on corresponding templates.

--- blog/models.py
+++ blog/models.py
@@ -3,6 +3,7 @@ from django.db import models
 from django.contrib.auth.models import User 
 from django.urls import reverse
 from datetime import datetime, date
+from ckeditor.fields import RichTextField
 
 # Create your models here. 
 
@@ -24,7 +25,8 @@ class Post(models.Model):
     title_tag = models.CharField(max_length=255)#, default="eCommerce Blog")
     meta_tag = models.CharField(max_length=255, default="kaavi")
     author = models.ForeignKey(User, on_delete=models.CASCADE)
-    body = models.TextField() 
+    body = RichTextField(blank=True, null=True)  
+    #body = models.TextField() 
     post_date = models.DateField(auto_now_add=True)
     category = models.CharField(max_length=255, default="Scooter") 
     likes = models.ManyToManyField(User, related_name='blog_posts')
