fd1bf742b64d7d0769a7e36167978c0c474a7b52
Isidore DelPierro <fontadore@gmail.com>
2022-08-22 08:47:22 +0100

    Edit Profile Page Form
    -----------------------
    
    1) Create edit_profile_page in members.urls
    
    2) Create EditProfilePageView in members.views 
    
    3) Create edit_profile_page.html in members app
    
    4) Restrict edit access to only the correct user.

--- members/views.py
+++ members/views.py
@@ -9,6 +9,12 @@ from django.contrib.auth.views import PasswordChangeView
 from blog.models import Profile
 # Create your views here.
 
+class EditProfilePageView(generic.UpdateView):
+    model = Profile
+    template_name = 'registration/edit_profile_page.html'
+    fields = ['bio', 'profile_pic', 'website_url', 'facebook_url', 'twitter_url', 'instagram_url', 'pinterest_url', 'tiktok_url', 'youtube_url', 'blogger_url']
+    success_url = reverse_lazy('home')
+
 class ShowProfilePageView(DetailView):
     model = Profile
     template_name = 'registration/user_profile.html'
